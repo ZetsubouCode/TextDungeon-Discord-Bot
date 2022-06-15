49fe050196fa7dbc2333841c7c96229e7d4a6121
ZetsubouCode <kignatius80@gmail.com>
2022-06-15 20:18:27 +0700

    Add base coding

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from _env import ENV
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("we have logged in successfully as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run(ENV.TOKEN)
