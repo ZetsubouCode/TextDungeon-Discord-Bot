0294ce4330c8edc67567d17f7e008828b8655917
ZetsubouCode <kignatius80@gmail.com>
2022-06-18 23:40:38 +0700

    Add initial controller

---
+++ controller/Player.py
@@ -0,0 +1,17 @@
+class Player:
+    def get_by_id(id):
+
+    def get_all():
+        #TODO
+
+    def get_by_discord_name(name):
+        #TODO
+
+    def add(inventory_id:int, job_id:int, discord_name:str, exp:int, level:int, add_attack:int, add_defend:int, add_eva:int):
+        #TODO
+
+    def update_by_id(target_id:int, inventory_id:int, job_id:int, discord_name:str, exp:int, level:int,
+                    add_attack:int, add_defend:int, add_eva:int):
+    
+    def update_by_discord_name(target_discord_name:str, inventory_id:int, job_id:int, discord_name:str, exp:int, level:int,
+                                add_attack:int, add_defend:int, add_eva:int):
