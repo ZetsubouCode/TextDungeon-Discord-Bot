cb277b40642927d31a9179597a0fbee2b606f6c8
ZetsubouCode <kignatius80@gmail.com>
2022-06-19 01:02:25 +0700

    Create base logic

--- function/StartGame.py
+++ function/StartGame.py
@@ -1,9 +1,19 @@
-from ..controller.Player import Player as PlayerController
-from ..controller.Job import Job as JobController
-from ..controller.Inventory import Inventory as InventoryController
+from controller.Player import Player as PlayerController
+# from controller.Job import Job as JobController
+# from controller.Inventory import Inventory as InventoryController
 
 class StartGame:
-    def start_game(discord_name:str):
-        job = JobController.add()
-        data = PlayerController.add(job.id)
-        inventory = InventoryController.add()
+    async def start_game(client, message):
+        await message.channel.send('Choose a character : \n1. Raden Mandala\n2. Indrayani\n3. Raider\n4. Maling')
+
+        def check(m):
+            option=['1','2','3','4']
+            return m.content in option and m.channel == message.channel and m.author == message.author
+        try:
+            msg = await client.wait_for("message", check=check,timeout=60)
+            
+        except Exception as e:
+            print("time out!")
+        # job = JobController.add()
+        # data = PlayerController.add(job.id)
+        # inventory = InventoryController.add()
