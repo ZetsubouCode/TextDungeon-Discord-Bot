4266deb9363654a7779a2071b58c7b0705c20900
ZetsubouCode <kignatius80@gmail.com>
2022-06-19 23:10:36 +0700

    Updating command list

--- function/Command.py
+++ function/Command.py
@@ -5,11 +5,11 @@ class Command:
                 """
 
     def command_list():
-        return """!start => Start the game\n!help => Show all possible command\n!menu => Show all main menu option\n
-                !history => Check all run history\n!exit => Exit from the run and save it\n!surender => Force to finish the current run if any\n
-                #inventory => Check items in inventory\n
-                !gallery => Show List of all character with the lore description
-                """
+        return "**COMMAND LIST**\n>>> !start => Start the game\n!help => Show all possible command\n!menu => Show all main menu option\n"\
+                "!history => Check all run history\n!exit => Exit from the run and save it\n!surrender => Force to finish the current run if any\n"\
+                "#inventory => Check items in inventory\n"\
+                "!gallery => Show List of all character with the lore description"
+                
 
     def main_menu():
         return """!start\n!gallery\n!history\n!exit
