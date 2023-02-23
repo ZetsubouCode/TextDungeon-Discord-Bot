2946bd3af89db2ed98079058f2af09352e0ca82b
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-02-23 04:35:46 +0000

    Add trible dot to avoid error

--- controller/Player.py
+++ controller/Player.py
@@ -1,17 +1,28 @@
 class Player:
-    def get_by_id(id):
 
-    def get_all():
-        #TODO
+  def get_by_id(id):
+    ...
 
-    def get_by_discord_name(name):
-        #TODO
+  def get_all():
+    #TODO
+    ...
 
-    def add(inventory_id:int, job_id:int, discord_name:str, exp:int, level:int, add_attack:int, add_defend:int, add_eva:int):
-        #TODO
+  def get_by_discord_name(name):
+    #TODO
+    ...
 
-    def update_by_id(target_id:int, inventory_id:int, job_id:int, discord_name:str, exp:int, level:int,
-                    add_attack:int, add_defend:int, add_eva:int):
-    
-    def update_by_discord_name(target_discord_name:str, inventory_id:int, job_id:int, discord_name:str, exp:int, level:int,
-                                add_attack:int, add_defend:int, add_eva:int):
+  def add(inventory_id: int, job_id: int, discord_name: str, exp: int,
+          level: int, add_attack: int, add_defend: int, add_eva: int):
+    #TODO
+    ...
+
+  def update_by_id(target_id: int, inventory_id: int, job_id: int,
+                   discord_name: str, exp: int, level: int, add_attack: int,
+                   add_defend: int, add_eva: int):
+    ...
+
+  def update_by_discord_name(target_discord_name: str, inventory_id: int,
+                             job_id: int, discord_name: str, exp: int,
+                             level: int, add_attack: int, add_defend: int,
+                             add_eva: int):
+    ...
--- main.py
+++ main.py
@@ -3,24 +3,27 @@ from function.StartGame import StartGame as StartGameFunction
 from function.Command import Command as CommandFunction
 from _env import ENV
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
+
 
 @client.event
 async def on_ready():
-    print("we have logged in successfully as {0.user}".format(client))
+  print("we have logged in successfully as {0.user}".format(client))
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user or message.channel.name != 'bot-test':
-        return
-    
-    if message.content.startswith('!Command'):
-        await message.channel.send(CommandFunction.command_list())
+  if message.author == client.user or message.channel.name != 'bot-test':
+    return
 
-    if message.content.startswith('!menu'):
-        await message.channel.send(CommandFunction.main_menu())
+  if message.content.startswith('!Command'):
+    await message.channel.send(CommandFunction.command_list())
 
-    if message.content.startswith('!start'):
-        await StartGameFunction.start_game(client, message)
+  if message.content.startswith('!menu'):
+    await message.channel.send(CommandFunction.main_menu())
+
+  if message.content.startswith('!start'):
+    await StartGameFunction.start_game(client, message)
+
 
 client.run(ENV.TOKEN)
